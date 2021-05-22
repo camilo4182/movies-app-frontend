eb4660c618ee57608ce2fd74f3c67fc8326df7e4
Johan Camilo Cortes Ocampo <camilo4182@gmail.com>
2021-05-21 22:53:45 -0500

    test: register and edit tests done

--- test/component/specs/edit-movie.test.js
+++ test/component/specs/edit-movie.test.js
@@ -1,18 +1,18 @@
 import '@testing-library/jest-dom/extend-expect'
 import { fireEvent, render, screen } from '@testing-library/react'
 import React from 'react'
-import RegisterMovie from '../../../src/RegisterMovie'
+import MovieEdit from '../../../src/MovieEdit'
 import { BrowserRouter as Router } from "react-router-dom"
 
 test('The register component shows the title, the movie input fields like the movie title, the description and director of the movie', () => {
   render(<Router>
-            <RegisterMovie />
+            <MovieEdit />
           </Router>);
 
   expect(screen.getByRole('textbox', { name: /title/i })).toBeInTheDocument();
   expect(screen.getByRole('textbox', { name: /description/i })).toBeInTheDocument();
   expect(screen.getByRole('textbox', { name: /director/i })).toBeInTheDocument();
-  expect(screen.getByRole('button', { name: /register/i })).toBeInTheDocument();
-  expect(screen.getByText(/register/i)).toBeInTheDocument();
+  expect(screen.getByRole('button', { name: /save/i })).toBeInTheDocument();
+  expect(screen.getByText(/save/i)).toBeInTheDocument();
   expect(screen.getByText(/cancel/i)).toBeInTheDocument();
 });
